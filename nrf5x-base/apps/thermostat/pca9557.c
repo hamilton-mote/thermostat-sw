e9826328320f4b4c54c7f6e65db9133b574a1b99
Gabe Fierro <gtfierro225@gmail.com>
2017-11-17 11:01:37 -0800

    add pca impl

---
+++ nrf5x-base/apps/thermostat/pca9557.c
@@ -0,0 +1,50 @@
+#include "pca9557.h"
+
+static nrf_drv_twi_t* m_instance;
+
+void pca9557_init(pca9557_cfg_t* cfg, nrf_drv_twi_t* p_instance) {
+    m_instance = p_instance;
+
+    // configure OUTPUT setting to be 0
+    uint8_t command[] = {PCA9557_OUT_REG, 0x0};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+
+    // configure all pins as output
+    command[0] = PCA9557_CFG_REG;
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+
+void heat_on(pca9557_cfg_t* cfg) {
+    uint8_t command[] = {PCA9557_OUT_REG, W_ON};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+    nrf_delay_us(15000);
+    command[1] = 0x0;
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+void heat_off(pca9557_cfg_t* cfg) {
+    uint8_t command[] = {PCA9557_OUT_REG, W_OFF};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+    nrf_delay_us(15000);
+    command[1] = 0x0;
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+
+void cool_on(pca9557_cfg_t* cfg) {
+    uint8_t command[] = {PCA9557_OUT_REG, Y_ON};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+void cool_off(pca9557_cfg_t* cfg) {
+    uint8_t command[] = {PCA9557_OUT_REG, Y_OFF};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+
+
+void fan_on(pca9557_cfg_t* cfg) {
+    uint8_t command[] = {PCA9557_OUT_REG, G_ON};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+void fan_off(pca9557_cfg_t* cfg) {
+    uint8_t command[] = {PCA9557_OUT_REG, G_OFF};
+    nrf_drv_twi_tx(m_instance, cfg->address, command, sizeof(command), false);
+}
+
---
+++ nrf5x-base/apps/thermostat/pca9557.h
@@ -0,0 +1,36 @@
+#ifndef _PCA9557_H
+#define _PCA9557_H
+
+#include <stdint.h>
+#include "nrf_drv_twi.h"
+#include <stdbool.h>
+#include <math.h>
+#include "nrf_delay.h"
+#include "thermostat.h"
+
+#define PCA9557_ADDR 24
+#define PCA9557_IN_REG 0x0
+#define PCA9557_OUT_REG 0x1
+#define PCA9557_POL_REG 0x2
+#define PCA9557_CFG_REG 0x3
+
+#define W_ON  (0x1)
+#define W_OFF (0x1 << 1)
+#define Y_ON  (0x1 << 2)
+#define Y_OFF (0x1 << 3)
+#define G_ON  (0x1 << 4)
+#define G_OFF (0x1 << 5)
+
+typedef struct pca9557_cfg_t {
+    uint8_t address;
+} pca9557_cfg_t;
+
+void pca9557_init(pca9557_cfg_t* cfg, nrf_drv_twi_t* p_instance);
+void heat_on(pca9557_cfg_t* cfg);
+void heat_off(pca9557_cfg_t* cfg);
+void cool_on(pca9557_cfg_t* cfg);
+void cool_off(pca9557_cfg_t* cfg);
+void fan_on(pca9557_cfg_t* cfg);
+void fan_off(pca9557_cfg_t* cfg);
+
+#endif // _PCA9557_H
